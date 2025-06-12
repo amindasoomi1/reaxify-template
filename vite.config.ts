154b0afa8cca12ff114127fd1c5bb2e196539459
amindasoomi1 <amindasoomi1@gmail.com>
2025-06-12 21:18:05 +0330

    svgr

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    svgr(),
     checker({
       typescript: true,
       // eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' }, // Optional
