ca5042fba04ca80a3843b3ccd45bd0731a5c4162
Amin dasoomi <amindasoomi1@gmail.com>
2026-02-09 12:52:47 +0330

    rules

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@ export { default as appConfig } from "./appConfig";
 export { default as languages } from "./languages";
 export { default as navItems } from "./navItems";
 export { default as queryKeys } from "./queryKeys";
-export { default as rules } from "./rules";
+export * as rules from "./rules";
--- src/constants/rules.ts
+++ src/constants/rules.ts
@@ -1,27 +1,21 @@
 import { Rules } from "react-form-rules";
 
-const required: Rules = [
+export const required: Rules = [
   (val) => {
     return !!String(val ?? "").trim().length || "Field is required.";
   },
 ];
-const emailRegex =
+export const emailRegex =
   // eslint-disable-next-line
   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const email: Rules = [(val) => emailRegex.test(val) || "Email is not valid."];
-const percent: Rules = [
+export const email: Rules = [
+  (val) => emailRegex.test(val) || "Email is not valid.",
+];
+export const percent: Rules = [
   (val) =>
     (Number(val) > 0 && Number(val) <= 100) ||
     "Percent should be between 0 and 100.",
 ];
-const verifyCode: Rules = [
+export const verifyCode: Rules = [
   (val) => val.length === 6 || "Please enter a valid 6-digit code.",
 ];
-
-const rules = {
-  required,
-  email,
-  percent,
-  verifyCode,
-};
-export default rules;
