c4f73102e5ee939c8815a535cb5e0519b75c083e
Amin dasoomi <amindasoomi1@gmail.com>
2026-05-07 11:44:08 +0330

    confirm service

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,7 @@
 import { Icon } from "@/components";
 import { Layout } from "@/layouts";
-import { confirm } from "@/services/confirm";
 import { IconName } from "@/types";
-import { Button, Card, Typography } from "reaxify/components";
+import { Card, Typography } from "reaxify/components";
 import { cn } from "reaxify/helpers";
 
 export default function Home() {
@@ -40,12 +39,6 @@ export default function Home() {
       colorClasses: "text-secondary",
     },
   ];
-  const test = async () => {
-    const result = await confirm()
-    if (result.confirmed) console.log('Success');
-    else console.log(result)
-
-  }
   return (
     <Layout>
       <Layout.Body>
@@ -83,9 +76,6 @@ export default function Home() {
               </Card.Body>
             </Card>
           ))}
-          <Button type="button" onClick={test}>
-            Confirm
-          </Button>
         </section>
       </Layout.Body>
     </Layout>
