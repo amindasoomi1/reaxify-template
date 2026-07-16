040590f177fb19fdf197413b2eab859bb2220e00
Amin dasoomi <amindasoomi1@gmail.com>
2026-07-16 22:24:28 +0330

    view transition

--- src/infra/view-transition/index.tsx
+++ src/infra/view-transition/index.tsx
@@ -2,3 +2,4 @@ export { default as startViewTransition } from "./startViewTransition";
 export { default as useViewTransition } from "./useViewTransition";
 export { default as ViewTransition } from "./ViewTransition";
 export { default as viewTransitionName } from "./viewTransitionName";
+import "./view-transition.css";
